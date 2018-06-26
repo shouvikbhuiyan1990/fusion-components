e2163d02ffdcad427279245be0629129bae37d35
Ambalika <shouvikbhuiyan1990@gmail.com>
2018-06-26 10:59:51 +0530

    Converting Carousel to a functional component

--- fusion/Carousel.js
+++ fusion/Carousel.js
@@ -1,123 +1,123 @@
 import { css } from 'emotion';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
-const carouselContainer = css`
-  width: 100%;
-  position: relative;
-`;
+const Carousel = ({
+  carouselData,
+  leftNavigation,
+  rightNavigation,
+  height,
+}) => {
+  const carouselContainer = css`
+    width: 100%;
+    position: relative;
+  `;
 
-const slidersContainer = css`
-  position: relative;
-  padding: 0;
-`;
+  const slidersContainer = css`
+    position: relative;
+    padding: 0;
+    height: ${height};
+  `;
 
-const slides = css`
-  position: relative;
-  list-style: none;
-  display: none;
-`;
+  const slides = css`
+    position: relative;
+    list-style: none;
+    display: none;
+  `;
 
-const slidesActive = css`
-  position: relative;
-  list-style: none;
-  display: block;
-`;
+  const slidesActive = css`
+    position: relative;
+    list-style: none;
+    display: block;
+  `;
 
-const imgResponsive = css`
-  min-width: 100%;
-`;
+  const imgResponsive = css`
+    min-width: 100%;
+    max-height: ${height};
+    min-height: ${height};
+  `;
 
-const leftArrow = css`
-  position: absolute;
-  z-index: 2;
-  top: 42%;
-  left: 0;
-`;
+  const leftArrow = css`
+    position: absolute;
+    z-index: 2;
+    top: 42%;
+    left: 0;
+  `;
 
-const rightArrow = css`
-  position: absolute;
-  z-index: 2;
-  top: 42%;
-  right: 0;
-`;
+  const rightArrow = css`
+    position: absolute;
+    z-index: 2;
+    top: 42%;
+    right: 0;
+  `;
 
-const arrowLeft = css`
-  cursor: pointer;
-  border: solid black;
-  border-width: 0 3px 3px 0;
-  display: inline-block;
-  padding: 6px;
-  transform: rotate(135deg);
-  -webkit-transform: rotate(135deg);
-`;
+  const arrowLeft = css`
+    cursor: pointer;
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 6px;
+    transform: rotate(135deg);
+    -webkit-transform: rotate(135deg);
+  `;
 
-const arrowRight = css`
-  cursor: pointer;
-  border: solid black;
-  border-width: 0 3px 3px 0;
-  display: inline-block;
-  padding: 6px;
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-`;
+  const arrowRight = css`
+    cursor: pointer;
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 6px;
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+  `;
 
-class Carousel extends Component {
-  constructor(props) {
-    super(props);
-    this.handleNavigation = this.handleNavigation.bind(this);
-  }
-
-  getActiveSlideStyle(selected) {
+  const getActiveSlideStyle = selected => {
     if (selected) {
       return slidesActive;
     }
     return slides;
-  }
+  };
 
-  handleNavigation(index, identifier) {
+  const handleNavigation = (index, identifier) => {
     if (identifier.toLowerCase() === 'left') {
-      this.props.leftNavigation(index);
+      leftNavigation(index);
     } else if (identifier.toLowerCase() === 'right') {
-      this.props.rightNavigation(index);
+      rightNavigation(index);
     }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className={carouselContainer}>
-          <ul className={slidersContainer}>
-            {!!this.props.carouselData &&
-              this.props.carouselData.map(({ title, url, selected }, i) => (
-                <li key={title} className={this.getActiveSlideStyle(selected)}>
-                  <div
-                    className={leftArrow}
-                    onClick={() => this.handleNavigation(i, 'left')}
-                    onKeyPress={() => {}}
-                    role="button"
-                    tabIndex={0}
-                  >
-                    <i className={arrowLeft} />
-                  </div>
-                  <div
-                    className={rightArrow}
-                    onClick={() => this.handleNavigation(i, 'right')}
-                    onKeyPress={() => {}}
-                    role="button"
-                    tabIndex={0}
-                  >
-                    <i className={arrowRight} />
-                  </div>
-                  <img className={imgResponsive} src={url} alt={title} />
-                </li>
-              ))}
-          </ul>
-        </div>
+  };
+  return (
+    <div>
+      <div className={carouselContainer}>
+        <ul className={slidersContainer}>
+          {!!carouselData &&
+            carouselData.map(({ title, url, selected }, i) => (
+              <li key={title} className={getActiveSlideStyle(selected)}>
+                <div
+                  className={leftArrow}
+                  onClick={() => handleNavigation(i, 'left')}
+                  onKeyPress={() => {}}
+                  role="button"
+                  tabIndex={0}
+                >
+                  <i className={arrowLeft} />
+                </div>
+                <div
+                  className={rightArrow}
+                  onClick={() => handleNavigation(i, 'right')}
+                  onKeyPress={() => {}}
+                  role="button"
+                  tabIndex={0}
+                >
+                  <i className={arrowRight} />
+                </div>
+                <img className={imgResponsive} src={url} alt={title} />
+              </li>
+            ))}
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Carousel.propTypes = {
   carouselData: PropTypes.arrayOf(
@@ -128,6 +128,7 @@ Carousel.propTypes = {
   ),
   leftNavigation: PropTypes.func.isRequired,
   rightNavigation: PropTypes.func.isRequired,
+  height: PropTypes.string,
 };
 
 Carousel.defaultProps = {
@@ -143,6 +144,7 @@ Carousel.defaultProps = {
       title: '2',
     },
   ],
+  height: 'auto',
 };
 
 export default Carousel;
